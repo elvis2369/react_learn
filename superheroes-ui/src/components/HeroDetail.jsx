a078bb0a664a4d84cd4d03458be4ebd21c1f1451
elvis2369 <mircea_pantazi@yahoo.com>
2023-07-31 14:39:16 +0100

    lab2

---
+++ superheroes-ui/src/components/HeroDetail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroDetail = () =>{
+    return(
+        <div>
+<img src={"images/batman.jpg"} alt="Batman" height="250px"/>
+<p id="blue-alias">Alias: Batman</p>
+<p >Name: Bruce Wayne</p>
+<button >Like</button>   
+        </div>
+    )
+}
+
+export default HeroDetail;
